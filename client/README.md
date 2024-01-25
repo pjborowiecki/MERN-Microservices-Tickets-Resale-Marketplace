8ae20ec81c943eeb522d327b2c5495c4ce92d04f
P J Borowiecki <piotr.borowiecki@yahoo.com>
2024-01-25 14:40:23 +0100

    Set up a client with Next.js

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# TicketThing Client
---
+++ client/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "@/styles/globals.css";
+
+import * as React from "react";
+import type { Metadata } from "next";
+import { fontInter } from "@/config/fonts";
+
+export const metadata: Metadata = {
+  title: "TicketThing",
+  description: "Event tickets resale marketplace",
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en">
+      <body className={fontInter.className}>{children}</body>
+    </html>
+  );
+}
